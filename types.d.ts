bd1e960c0d3f22f8867f313797bc3667b3023d1d
Jacob Chademwiri <jchademwiri@gmail.com>
2023-04-10 10:31:16 +0200

    added sitemap and robots.txt

---
+++ types.d.ts
@@ -0,0 +1,21 @@
+type Robots = {
+  rules:
+    | {
+        userAgent?: string | string[];
+        allow?: string | string[];
+        disallow?: string | string[];
+        crawlDelay?: number;
+      }
+    | Array<{
+        userAgent: string | string[];
+        allow?: string | string[];
+        disallow?: string | string[];
+        crawlDelay?: number;
+      }>;
+  sitemap?: string | string[];
+  host?: string;
+};
+type Sitemap = Array<{
+  url: string;
+  lastModified?: string | Date;
+}>;
